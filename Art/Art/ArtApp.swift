3a7f158e1292197f820abf00dcd68bfc6dff2a4e
Leffin <levfag@gmail.com>
2025-07-07 08:40:15 +0700

    init commit

---
+++ Art/Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by Effin Leffin on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
