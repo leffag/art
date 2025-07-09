812c3fd9f053a070f108b2a02531987f9b56ec74
Leffin <levfag@gmail.com>
2025-07-09 11:18:55 +0700

    tmp

---
+++ Art/Art/WorkView.swift
@@ -0,0 +1,21 @@
+//
+//  WorkView.swift
+//  Art
+//
+//  Created by Effin Leffin on 08.07.2025.
+//
+
+import SwiftUI
+
+struct WorkView: View {
+    let work: Work
+    
+    var body: some View {
+        Image(work.image)
+            .aspectRatio(contentMode: .fill)
+    }
+}
+
+#Preview {
+    WorkView(work: parseArtists()[0].works[0])
+}
