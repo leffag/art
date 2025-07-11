c281e5de4a13d16a30e67fdd9001bcdd94fa4312
Leffin <levfag@gmail.com>
2025-07-11 20:33:36 +0700

    ArtistInfoView
    исправлено отображение текста

--- Art/Art/ArtistInfoView.swift
+++ Art/Art/ArtistInfoView.swift
@@ -11,23 +11,32 @@ struct ArtistInfoView: View {
     let artist: Artist
     
     var body: some View {
-        ScrollView{
-            Image(artist.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
+        ScrollView {
+            VStack {
+                Image(artist.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
                 //TODO: ignoreSaveArea
-            Text("Biography")
-                .font(.title)
-            Text(artist.bio)
-                .font(.title2)
-                .foregroundStyle(.gray)
-            ForEach(artist.works, id: \.title) { work in
-                NavigationLink() {
-                    WorkInfoView(work: work)
-                } label: {
-                    VStack{
-                        Image(work.image)
-                        Text(work.title)
+                
+                Text("Biography")
+                    .font(.title)
+                
+                Text(artist.bio)
+                    .font(.title2)
+                    .foregroundStyle(.gray)
+                
+                Text("Works")
+                
+                ForEach(artist.works, id: \.title) { work in
+                    NavigationLink() {
+                        WorkInfoView(work: work)
+                    } label: {
+                        VStack {
+                            Image(work.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                            Text(work.title)
+                        }
                     }
                 }
             }
@@ -35,6 +44,6 @@ struct ArtistInfoView: View {
     }
 }
 
-//#Preview {
-//    ArtistInfoView(artist: parseArtists()[0])
-//}
+#Preview {
+    ArtistInfoView(artist: parseArtists()[1])
+}
